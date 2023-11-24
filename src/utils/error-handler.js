42f53f31e2b4cb0ee7452c6c9dcd22f762b6aa8a
Ankan Roychowdhury <roychowdhuryankan2001@gmail.com>
2023-11-24 23:28:45 +0530

    updated error handling

---
+++ src/utils/error-handler.js
@@ -0,0 +1,17 @@
+import { StatusCodes } from 'http-status-codes';
+
+export default class AppError extends Error {
+    constructor(
+        name = 'AppError',
+        message = 'Something went wrong',
+        explanation = 'Something went wrong',
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+    )
+    {
+        super();
+        this.name = name,
+        this.message = message,
+        this.explanation = explanation,
+        this.statusCode = statusCode
+    }
+}
