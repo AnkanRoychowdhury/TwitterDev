f69c76d22d6799a8c3a09bae4257677ae64f895a
Ankan Roychowdhury <roychowdhuryankan2001@gmail.com>
2023-11-28 18:39:50 +0530

    fix: exports

---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+import CommentService from "./comment-service.js";
+import TweetService from "./tweet-service.js";
+import ReactionService from "./reaction-service.js";
+import UserService from "./user-service.js";
+
+export {
+    CommentService,
+    TweetService,
+    ReactionService,
+    UserService
+}
